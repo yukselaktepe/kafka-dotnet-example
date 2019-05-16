df6ec081512d52be34a73e9dea28a9b067b56a5b
Yüksel Aktepe <yukselaktepe@hotmail.com.tr>
2019-05-16 15:37:49 +0300

    Initial commit

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Consumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var brokerList = "localhost:9092";
+            var topics = new List<string> { "example" };
+            Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+
+            {
+                e.Cancel = true; 
+                cts.Cancel();
+            };
+
+            Run_ManualAssign(brokerList, topics, cts.Token);
+
+        }
+
+        public static void Run_ManualAssign(string broker, List<string> topics, CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = new Guid().ToString(),
+                BootstrapServers = broker,
+                EnableAutoCommit = true
+            };
+
+            using (var consumer =
+                new ConsumerBuilder<Ignore, string>(config)
+                    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                    .Build())
+            {
+                consumer.Assign(topics.Select(topic => new TopicPartitionOffset(topic, 0, Offset.Beginning)).ToList());
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumeResult = consumer.Consume(cancellationToken);
+                            Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: ${consumeResult.Value}");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Consume error: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Closing consumer.");
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,62 @@
+﻿using Confluent.Kafka;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brokerList = "localhost:9092";
+            string topicName = "example";
+
+            var config = new ProducerConfig { BootstrapServers = brokerList };
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                Console.WriteLine($"Producer {producer.Name} producing on topic {topicName}.");
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("> value<enter>");
+                Console.WriteLine("Ctrl-C to quit.\n");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; 
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    Console.Write("> ");
+
+                    string text;
+                    try
+                    {
+                        text = Console.ReadLine();
+                    }
+                    catch (IOException)
+                    {
+                        break;
+                    }
+                    if (text == null)
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                            producer.Produce(topicName, new Message<string, string> { Key = "", Value = text });
+                    }
+                    catch (ProduceException<string, string> e)
+                    {
+                        Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                    }
+                }
+            }
+        }
+    }
+}
